d865aa18551dddd1fa0042af76fe211e6451fbac
Juan Carlos Moreno Guerra <jcmg.ing@gmail.com>
2023-09-26 14:49:01 -0500

    cambios

---
+++ src/pages/Loan/Index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+import Form from '@/pages/Loan/Components/Form.vue';
+import TableList from '@/pages/Loan/Components/TableList.vue';
+import { useCrudLoanStore } from '@/pages/Loan/Store/useCrudLoanStore';
+const storeLoan = useCrudLoanStore()
+const { typeAction } = storeToRefs(storeLoan)
+
+onUnmounted(() => {
+  storeLoan.$reset();
+})
+
+</script>
+
+<template>
+  <div>
+    <TableList v-show="typeAction !== 'form'" :key="storeLoan.keyList" />
+    <Form v-show="typeAction === 'form'"></Form>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  action: read
+  subject: Auth
+  redirectIfLoggedIn: true
+  requiresAuth: true
+  requiredPermission: loan.index
+</route>
+
---
+++ src/pages/Product/Index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+import Form from '@/pages/Product/Components/Form.vue';
+import TableList from '@/pages/Product/Components/TableList.vue';
+import { useCrudProductStore } from '@/pages/Product/Store/useCrudProductStore';
+const storeProduct = useCrudProductStore()
+const { typeAction } = storeToRefs(storeProduct)
+
+onUnmounted(() => {
+  storeProduct.$reset();
+})
+
+</script>
+
+<template>
+  <div>
+    <TableList v-show="typeAction !== 'form'" :key="storeProduct.keyList" />
+    <Form v-show="typeAction === 'form'"></Form>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  action: read
+  subject: Auth
+  redirectIfLoggedIn: true
+  requiresAuth: true
+  requiredPermission: product.index
+</route>
+
